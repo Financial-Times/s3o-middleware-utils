8460322c5b95619a24f897abf95193523f38eed2
Ændrew Rininsland <andrew.rininsland@ft.com>
2017-02-08 13:53:53 +0000

    Scaffolding some tests

---
+++ test/authenticate.spec.js
@@ -0,0 +1,11 @@
+/**
+ * Basic spec for lib/authenticate.js
+ */
+
+const chai = require('chai');
+const sinon = require('sinon');
+const sinonChai = require('sinon-chai');
+const proxyquire = require('proxyquire');
+
+chai.should();
+chai.use(sinonChai);
