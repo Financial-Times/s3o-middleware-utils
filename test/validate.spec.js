0d3bc6a82c302501b2e5d67d1870777181af0892
Charlie Briggs <charlie.briggs@ft.com>
2019-03-21 18:07:38 +0000

    Review comments
    Fix typos, make naming clearer

--- test/validate.spec.js
+++ test/validate.spec.js
@@ -16,8 +16,8 @@ chai.use(sinonChai);
 // https://github.com/Financial-Times/s3o/blob/992be3a122006210013da64aa2ceee65e03a479d/src/main/java/com/ft/s3o/util/KeyGenerator.java#L19
 const generateRSAKeyPair = () => new NodeRSA().generateKeyPair(512);
 
-const sign = (data, key) => {
-	const privatePem = key.exportKey('pkcs8-private-pem');
+const sign = (data, keyPair) => {
+	const privatePem = keyPair.exportKey('pkcs8-private-pem');
 	const s = crypto.createSign('sha1');
 	s.update(data);
 	s.end();
@@ -28,11 +28,11 @@ const keyFactory = sinon.stub();
 
 describe('lib/validate.js', () => {
 	let publicKey
-	let key
+	let keyPair
 
 	before(() => {
-		key = generateRSAKeyPair(4096)
-		publicKey = key.exportKey('pkcs8-public-der');
+		keyPair = generateRSAKeyPair(4096)
+		publicKey = keyPair.exportKey('pkcs8-public-der');
 	})
 
 	beforeEach(() => {
@@ -47,7 +47,7 @@ describe('lib/validate.js', () => {
 
 	describe('validator function', () => {
 		['', false, undefined, null].forEach((value) => {
-			it(`returns false if publicKey is ${value}`, () => {
+			it(`returns false if publicKey is "${value}"`, () => {
 				keyFactory.returns(value);
 
 				const validator = validatorFactory(keyFactory);
@@ -59,7 +59,7 @@ describe('lib/validate.js', () => {
 		});
 
 		it('returns false if the supplied key is empty', () => {
-			const token = sign('', key)
+			const token = sign('', keyPair)
 
 			const validator = validatorFactory(keyFactory);
 			const result = validator('charlie.briggs', token);
@@ -68,8 +68,8 @@ describe('lib/validate.js', () => {
 			result.should.equal(false);
 		});
 
-		it('returns false if supplied key does not validates against public key', () => {
-			const token = sign('charlie.briggs-lantern.ft.com', key)
+		it('returns false if supplied key does not validate against public key', () => {
+			const token = sign('charlie.briggs-lantern.ft.com', keyPair)
 
 			const validator = validatorFactory(keyFactory);
 			const result = validator('charlie.briggs', token);
@@ -79,7 +79,7 @@ describe('lib/validate.js', () => {
 		});
 
 		it('returns true if supplied key validates against the public key', () => {
-			const token = sign('charlie.briggs:lantern.ft.com', key)
+			const token = sign('charlie.briggs-lantern.ft.com', keyPair)
 
 			const validator = validatorFactory(keyFactory);
 			const result = validator('charlie.briggs-lantern.ft.com', token);
