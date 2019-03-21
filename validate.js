5eb79e0980b5202f90dc417c2e5afd048ca9eec9
Charlie Briggs <charlie.briggs@ft.com>
2019-03-21 18:11:29 +0000

    Rename key to object, token to signature as per node crypto docs
    See 'https://nodejs.org/api/crypto.html#crypto_verify_verify_object_signature_signatureencoding'
    
    This is a generic method for verifying some string against a signature, we don't know what format the object is in this function

--- validate.js
+++ validate.js
@@ -5,13 +5,13 @@ const crypto = require('crypto');
 const NodeRSA = require('node-rsa');
 
 /**
- * Factory function for creating token validators
+ * Factory function for creating cryptographic signature validators
  *
  * @param  {string} s3oPublicKey S3O public key from public key service
  * @return {function}            Returns validator function
  */
 module.exports = function (s3oPublicKey) {
-	return function (key, signedToken) {
+	return function (object, signature) {
 		const publicKey = s3oPublicKey();
 		if (!publicKey) {
 			return false;
@@ -20,13 +20,13 @@ module.exports = function (s3oPublicKey) {
 		// Convert the publicKey from DER format to PEM format
 		// See: https://www.npmjs.com/package/node-rsa
 		const buffer = new Buffer(publicKey, 'base64');
-		const derKey = new NodeRSA(buffer, 'pkcs8-public-der');
-		const publicPem = derKey.exportKey('pkcs8-public-pem');
+		const publicDer = new NodeRSA(buffer, 'pkcs8-public-der');
+		const publicPem = publicDer.exportKey('pkcs8-public-pem');
 
 		// See: https://nodejs.org/api/crypto.html
 		const verifier = crypto.createVerify('sha1');
-		verifier.update(key);
+		verifier.update(object);
 
-		return verifier.verify(publicPem, signedToken, 'base64');
+		return verifier.verify(publicPem, signature, 'base64');
 	};
 };
